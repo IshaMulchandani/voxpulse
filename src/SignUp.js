c191f66c36e068d8714a17d6f26fc18aa8ec9970
vvaish0987 <vvaish0987@gmail.com>
2025-06-27 10:03:38 +0530

    Changes

--- src/SignUp.js
+++ src/SignUp.js
@@ -7,49 +7,53 @@ export default function SignUp(){
         <div className="signUpCont">
             <h1>Sign Up</h1>
             <form action="">
-                <label htmlFor="name">Name</label><br />
-                <input type="text" required placeholder="Enter name..."/>
-                <br /><br />
+                <label htmlFor="name">Name</label>
+                <input type="text" required placeholder="Enter your full name..."/>
+                
                 <label htmlFor="email_id">Email ID</label>
-                <br />
-                <input type="email" required placeholder="Enter email address..." />
-                <br /><br />
+                <input type="email" required placeholder="Enter your email address..." />
+                
                 <label htmlFor="phoneNo">Phone Number</label>
-                <br />
-                <input type="number" required placeholder="Enter phone number..." />
-                <br /><br />
+                <input type="number" required placeholder="Enter your phone number..." />
+                
                 <label htmlFor="password">Create Password</label>
-                <br />
-                <input type="password" required placeholder="Create a new password..." />
-                <br /><br />
-                <label htmlFor="gender">Choose your gender:</label><br />
-                <label htmlFor="male">Male
-                    <input type="radio" name="gender" value='male' />
-                </label><br />
-                <label htmlFor="female">Female
-                    <input type="radio" name="gender" value='female' />
-                </label><br />
-                <label htmlFor="other">Other
-                    <input type="radio" name="gender" value='other' />
-                </label><br /><br />
+                <input type="password" required placeholder="Create a secure password..." />
+                
+                <div className="gender-section">
+                    <label htmlFor="gender">Choose your gender:</label>
+                    <label htmlFor="male">
+                        <input type="radio" name="gender" id="male" value='male' />
+                        Male
+                    </label>
+                    <label htmlFor="female">
+                        <input type="radio" name="gender" id="female" value='female' />
+                        Female
+                    </label>
+                    <label htmlFor="other">
+                        <input type="radio" name="gender" id="other" value='other' />
+                        Other
+                    </label>
+                </div>
+                
                 <label htmlFor="dob">Date of Birth</label>
-                <br />
-                <input type="date" required/><br /><br />
+                <input type="date" id="dob" required/>
+                
                 <label htmlFor="profession">Profession</label>
-                <br />
-                <input type="text" required placeholder="Enter your profession..." />
-                <br /><br />
+                <input type="text" id="profession" required placeholder="Enter your profession..." />
+                
                 <label htmlFor="annualIncome">Annual Income</label>
-                <br />
-                <select name="" id="">
-                    <option value="" selected disabled>Select</option>
-                    <option value="">0- &#8377;1,00,000</option>
-                    <option value="">&#8377;1,00,001-&#8377;3,00,000</option>
-                    <option value="">&#8377;3,00,001-&#8377;6,00,000</option>
-                    <option value="">&#8377;6,00,001-&#8377;10,00,000</option>
-                    <option value="">&gt;&#8377;10,00,000</option>
-                </select><br /><br />
-                <Link to='/' ><button>Submit</button></Link>
+                <select name="annualIncome" id="annualIncome">
+                    <option value="" selected disabled>Select your income range</option>
+                    <option value="0-100000">₹0 - ₹1,00,000</option>
+                    <option value="100001-300000">₹1,00,001 - ₹3,00,000</option>
+                    <option value="300001-600000">₹3,00,001 - ₹6,00,000</option>
+                    <option value="600001-1000000">₹6,00,001 - ₹10,00,000</option>
+                    <option value="1000000+">₹10,00,000+</option>
+                </select>
+                
+                <Link to='/' >
+                    <button type="button">Submit</button>
+                </Link>
             </form>
         </div>
     )
